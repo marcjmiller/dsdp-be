e403533f601d5d6d6f916f1c4050f08034dba675
Ethan Pellegrini <ethan.p.pellegrini.civ@army.mil>
2021-11-22 09:24:32 -0500

    Adds minio dependency and sets up endpoint to connect to minio

--- src/api/routes/files.py
+++ src/api/routes/files.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
-
+from minio import Minio
+import os
 
 router = APIRouter()
 
+client = Minio(os.getenv("MINIO_URL"), os.getenv("MINIO_ACCESS_KEY"), os.getenv("MINIO_SECRET_KEY"))
 
-@router.get("", name="files:hello")
-async def hello():
-  return {"data": "Hello, World!"}
+
+@router.get("", name="files:list")
+async def list():
+  return client.list_buckets()
--- src/tests/test_files.py
+++ src/tests/test_files.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from starlette.status import HTTP_404_NOT_FOUND
+
+
 class TestFilesRoutes:
-  def test_routes_exist(self, app: FastAPI, client: TestClient):
-    res = client.get(app.url_path_for("files:hello"))
+  def test_get_bucket_name(self, app: FastAPI, client: TestClient):
+    res = client.get(app.url_path_for("files:list"))
     assert res.status_code != HTTP_404_NOT_FOUND
-    
+  
