19501518ebbd4dbf58d0752f6d373ee4bc225ef7
abutt <anthony.butt@cse-corp.com>
2021-11-23 13:27:35 -0500

    Allow CORS for Development purposes

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes.api import router as api_router
 
 
 def get_application() -> FastAPI:
   application = FastAPI()
+  origins = [
+    "http://localhost:3000"
+  ]
+
+  application.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+  )
+
   application.include_router(api_router, prefix="/api")
   return application
 
