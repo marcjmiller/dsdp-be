665b9b03cbb8689917a82d8874d8a5fd3e62fe49
Marc Miller <marc.miller@cse-corp.com>
2022-01-12 11:04:33 -0500

    update auth.py to hopefully work, update files endpoint to wrap minio in try/except

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -1,16 +1,20 @@
 from typing import Optional
 from fastapi import APIRouter, Header
+from jwt import DecodeError
 
-import jwt
+import jwt, requests
 
 router = APIRouter()
 
 @router.get("/user/roles", name="auth:getUserRoles")
 def user_roles(authorization: Optional[str] = Header(None)):
+    pub_key = ""
+    client_id = ""
     try:
         encoded_token = authorization.split(" ")[1]
-        pub_key = "keycloakPublicKey"
-        client_id = "someString"
+        baby_yoda = requests.get("https://login.dso.mil/auth/realms/baby-yoda/").json()
+        pub_key = f"-----BEGIN PUBLIC KEY-----\n{baby_yoda['public_key']}\n-----END PUBLIC KEY-----"
+        client_id = "il2_00eb8904-5b88-4c68-ad67-cec0d2e07aa6_mission-staging"
         algorithm = ["RS256"]
         decoded_token = jwt.decode(encoded_token, pub_key, client_id, algorithm)
 
@@ -22,6 +26,15 @@ def user_roles(authorization: Optional[str] = Header(None)):
           "groups": decoded_token["group-simple"]
         }
     except AttributeError:
-        return None
-
+        print(f"Attribute error, Auth: {authorization}")
+        return {"Message": "Attribute Error, see container logs"}
+    except KeyError:
+        print(f"Key error, \nPubKey: {pub_key} \nAuth: {authorization}")
+        return {"Message:": "Invalid KEYCLOAK_PUBLIC_KEY, see container logs"}
+    except DecodeError:
+        print(f"Decode error, Auth: {authorization}, Key {pub_key}, Audience: {client_id}")
+        return {"Message": "Decode Error, see container logs"}
+    except:
+        print(f"Auth:{authorization},\n Public Key: {pub_key},\n Client ID: {client_id}")
+        return {"Message": "Error, see container logs"}
     return user
--- api/routes/files.py
+++ api/routes/files.py
@@ -8,16 +8,23 @@ from starlette.responses import FileResponse
 router = APIRouter()
 
 BUCKET = os.getenv("MINIO_BUCKET_NAME") or "bucket"
-mc = Minio(
-    os.getenv("MINIO_URL") or "localhost:9000",
-    os.getenv("MINIO_ACCESS_KEY") or "minio",
-    os.getenv("MINIO_SECRET_KEY") or "minio123",
-    secure=False,
-)
+MINIO_HOST = os.getenv("MINIO_HOST") or "localhost"
+MINIO_PORT = os.getenv("MINIO_PORT") or "9000"
+MINIO_URL = os.getenv("MINIO_URL") or f"{MINIO_HOST}:{MINIO_PORT}"
 
-if not mc.bucket_exists(BUCKET):
-    mc.make_bucket(BUCKET)
+try:
+    mc = Minio(
+        MINIO_URL,
+        os.getenv("MINIO_ACCESS_KEY") or "minio",
+        os.getenv("MINIO_SECRET_KEY") or "minio123",
+        secure=False,
+    )
 
+    if not mc.bucket_exists(BUCKET):
+        mc.make_bucket(BUCKET)
+except Exception as exception:
+    print(f"Bucket: {BUCKET}, \nMinio_URL: {MINIO_URL}, \nException: {exception}")
+    raise exception
 
 @router.get("", name="files:getFile")
 async def download(name: str) -> FileResponse:
