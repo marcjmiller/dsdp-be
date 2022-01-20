0994a4660fd3f346c1988df531814ac06b36d3ef
Ethan Pellegrini <ethan.p.pellegrini.civ@army.mil>
2022-01-20 13:20:38 -0500

    Add auth test with decode mock
    Fix path issues for tests

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -1,8 +1,28 @@
 import pytest
+import requests
 from api.routes.auth import user_roles
 from mock import MagicMock
 
 def test_auth(mocker):
-    mock = mocker.patch("backend.api.routes.auth.jwt.decode", MagicMock())
-    user_roles()
-    mock.assert_called_with("", "", "", "")
+    return_value={
+        "name": "user name",
+        "preferred_username": "user.name",
+        "email": "useremail",
+        "activecac": "",
+        "group-full": ["Products-Teams/Dsdp", "/Product-Teams/Dsdp/Admin"],
+    }
+    expected_user_obj = {
+        "name": "user name",
+        "username": "user.name",
+        "email": "useremail",
+        "cac": "",
+        "groups": ["Products-Teams/Dsdp", "/Product-Teams/Dsdp/Admin"],
+        "isAdmin": True
+    }
+    mock = mocker.patch("api.routes.auth.decode", MagicMock())
+    mock.return_value = return_value
+    baby_yoda = requests.get("https://login.dso.mil/auth/realms/baby-yoda/").json()
+    pub_key = f"-----BEGIN PUBLIC KEY-----\n{baby_yoda['public_key']}\n-----END PUBLIC KEY-----"
+    user_obj = user_roles("bearer fakebearertoken")
+    mock.assert_called_with('fakebearertoken', pub_key, audience='il2_00eb8904-5b88-4c68-ad67-cec0d2e07aa6_mission-staging', algorithms=['RS256'])
+    assert user_obj==expected_user_obj
