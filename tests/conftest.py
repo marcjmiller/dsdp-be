aedd5a05ea6a6d70bd5a31827cfabf67da5e665e
harriscfn <clinton.harris@cse-corp.com>
2022-02-03 10:11:07 -0500

    Removes minio client from conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,18 +2,6 @@ import os
 import pytest_asyncio
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from minio import Minio
-
-MINIO_ENDPOINT = "minio:9000" if os.getenv("CI") else "localhost:9000"
-
-
-@pytest_asyncio.fixture(autouse=True)
-def init_minio():
-    c = Minio(MINIO_ENDPOINT, "minio", "minio123", secure=False)
-    if not c.bucket_exists("test"):
-        c.make_bucket("test")
-    yield
-
 
 @pytest_asyncio.fixture
 def app() -> FastAPI:
